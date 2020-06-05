447a5fbc90117326cacb72cda01685d836c138a4
wsmzjzz <wsmzjzz@gmail.com>
2020-06-05 18:12:25 +0800

    WordCloud Names

--- get_data.py
+++ get_data.py
@@ -1,6 +1,14 @@
 """ 这部分获取全书信息 """
 
 # import collections
+import re
+
+def find_chinese(file):
+    pattern = re.compile(r'[^\u4e00-\u9fa5]')
+    chinese = re.sub(pattern, '', file)
+    return chinese
+    # print(chinese)
+
 
 book_path = 'data/book.txt'
 names_path = 'data/names.txt'
@@ -16,6 +24,10 @@ with open(book_path, 'r') as book_object:
     """ get full text with no space """
     for line in book_object:
         book_text += line.strip()
+
+book_text = str(find_chinese(book_text))
+# print(book_text[19800:20000])
+
 # print(len(book_text))
 # print(book_text[5800:6000])
 # text = "你知道啥玩意儿"
@@ -23,24 +35,31 @@ with open(book_path, 'r') as book_object:
 # print(word_set)
 
 """ count character frequency (non-space) """
-word_exception = ['，', '。', '：', '“', '”', '？', '！', '.', '?', ',', '!']
+word_exception = ['‘']
 frequency = {} #-- 汉字出现频率
-word_set = [] #-- 汉字集合
+# word_set = [] #-- 汉字集合
+# word_list = []
 for word in book_text:
     if word not in word_exception:
+        # word_list.append(word)
         if word not in frequency:
             frequency[word] = 1
-            word_set.append(word)
+            # word_set.append(word)
         else:
             frequency[word] += 1
 # print(frequency['走'])
 # print(len(frequency))
 # print(word_set[200:250])
+# for word, times in frequency.items():
+#     if times > 6000:
+#         print(word)
 
 """ count how many times a name appears """
+name_string = ''
 appear_times = {} #-- 人名出现次数
 for name in names:
     appear_times[name] = book_text.count(name)
-
+    name_string += (name + ',') * appear_times[name]
 # for name, times in appear_times.items():
 #     print(name + ' appears ' + str(times) + ' times.')
+# print(name_string[10000:10200])
--- plot.py
+++ plot.py
@@ -1,12 +1,38 @@
 """ 这部分主管绘图 """
 
 #-- 导入处理好的数据
-from get_data import names, word_set, book_text
-from get_data import appear_times, frequency
+# from get_data import names, book_text, word_exception
+from get_data import appear_times, frequency, name_string
 #-- 导入绘图库
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+from wordcloud import WordCloud
+import jieba
+
+# space_text = ''
+# for word in book_text:
+#     space_text += word + ' '
+# jieba_words = []
+# word_generator = jieba.cut(book_text)
+# for chara in word_generator:
+#     if chara not in word_exception:
+#         jieba_words.append(chara.strip())
+# print(jieba_words[:50])
+
+# print(space_text[:100])
+# print(book_text[:100])
+# print(list(word_list)[:100])
+print(name_string[:200])
+
+c_font = '/System/Library/Fonts/PingFang.ttc'
+wordcloud = WordCloud(collocations=False,background_color="white",
+                        width=1000, height=860,
+                        font_path=c_font, margin=2).generate(name_string)
+
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.show()
 
 
 # input_1 = np.arange(0 ,math.pi * 2, 0.1)
@@ -16,7 +42,7 @@ import numpy as np
 # plt.plot(input_1, y1, label='y=sinx')
 # squares = [math.log(i) for i in range(2, 50)]
 # squares2 = [math.log10(i) for i in range(1, 10)]
-# plt.plot(input_1, squares, linewidth=5)
+# plt.plot(input_1, squar·es, linewidth=5)
 # plt.plot(names, appear_times.values(), linewidth=2, label='y=logx')
 # for i in range(len(input_1)):
 # plt.scatter(input_1, squares, edgecolors='red')
@@ -26,13 +52,29 @@ import numpy as np
 # plt.scatter(x,y, c=t)
 
 # print(appear_times['宋江'])
+# for name, times in appear_times.items():
+#     plt.scatter(name, times)
+# cnt_over = 0
+# cnt_lower = 0
+# lim = 20
+# for word, times in frequency.items():
+#     if times > lim:
+#         # plt.scatter(word, times)
+#         cnt_over += 1
+#     else :
+#         cnt_lower += 1
+# #     cnt += 1
+# #     if cnt > 500:
+# #         break
+# # print('cnt= ' + str(cnt))
 
-for name, times in appear_times.items():
-    plt.scatter(name, times)
+# # plt.legend(loc='upper right')
+# # plt.title('Logistic Curve', fontsize=20)
+# # plt.xlabel('x-Axis')
+# # plt.ylabel('y=log(x)')
+# # plt.tick_params(axis='x', labelsize=10)
+# # plt.title('All Men Are Brothers')
+# # plt.show()
 
-# plt.legend(loc='upper right')
-# plt.title('Logistic Curve', fontsize=20)
-# plt.xlabel('x-Axis')
-# plt.ylabel('y=log(x)')
-# plt.tick_params(axis='x', labelsize=10)
-plt.show()
+# print(f"{cnt_over} words over{lim}")
+# print(f"{cnt_lower} words lower{lim}")
